04cb097b3df2cc32702a97c0b5930811df75c8e5
Francisco Ripardo Maia Junior <57280475+FcoRipJr@users.noreply.github.com>
2026-09-03 20:07:55 +0000

    teste

--- pages/api/teste.js
+++ pages/api/teste.js
@@ -3,10 +3,10 @@ async function get(request, response) {
     const dados =
       request.method == "GET" ? request.query : await request.json();
     console.log(dados);
-    response.status(200).json({ status: true, request: dados });
+    response.status(200).json({ success: true, request: dados });
   } catch (error) {
     console.error(error);
-    response.status(200).json({ status: false, error: error });
+    response.status(200).json({ success: false, error: error });
   }
 }
 
